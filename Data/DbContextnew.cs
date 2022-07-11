4f5d9619e6c5eccfcd593461c875c24e9e1d72d1
Abhinay Reddy <abhinayreddyd7@gmail.com>
2022-07-11 10:12:07 -0500

    Adding Files

---
+++ Data/DbContextnew.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using sample_crud.Model;
+
+    public class DbContextnew : DbContext
+    {
+        public DbContextnew (DbContextOptions<DbContextnew> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<sample_crud.Model.Employee> Employee { get; set; }
+
+        public DbSet<sample_crud.Model.Persons> Persons { get; set; }
+    }
---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM mcr.microsoft.com/dotnet/sdk:6.0-bullseye-slim AS build-env
+WORKDIR /app
+
+COPY *.csproj ./
+RUN dotnet restore
+
+COPY . ./
+RUN dotnet publish -c release -o out
+
+FROM mcr.microsoft.com/dotnet/aspnet:6.0-bullseye-slim as final
+WORKDIR /app
+COPY --from=build-env /app/out .
+
+EXPOSE 8080
+ENV ASPNETCORE_URLS=http://*:8080
+
+ENTRYPOINT ["dotnet", "sample_crud.dll"]
