cb9366404997bf586d285ed2765bfd8265a83a3a
Abhinay Reddy <abhinayreddyd7@gmail.com>
2022-07-11 10:25:01 -0500

    typo in namespce

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 
-namespace sampledb_crud
+namespace sample_crud
 {
     public class Startup
     {
